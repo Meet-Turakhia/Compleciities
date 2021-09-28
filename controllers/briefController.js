9d3b98dadc5c911cf6669948a8a12fd56011192d
Meet-Turakhia <turakhiameeturesh@gmail.com>
2021-09-28 22:58:19 +0530

    added routing for brief

--- controllers/briefController.js
+++ controllers/briefController.js
@@ -5,11 +5,33 @@ var router = express.Router();
 const mongoose = require("mongoose");
 const Marker = mongoose.model("markers");
 const Brief = mongoose.model("briefs");
+const User = mongoose.model("user");
 
 // routes
 
-router.get("/", (req, res) => {
-    res.render("layouts/brief");
+router.get("/:markerTitle", (req, res) => {
+    markerTitle = req.params.markerTitle.replace("-", " ");
+
+    Brief.findOne({ marker_title: markerTitle }, (briefErr, briefDocs) => {
+        if (!briefErr) {
+            User.find((userErr, userDocs) => {
+                if (briefDocs != null) {
+                    if (!userErr) {
+                        res.render("layouts/brief", {
+                            briefData: briefDocs,
+                            userData: userDocs
+                        });
+                    } else {
+                        console.log("Following error occured while retrieving the user data: " + userErr);
+                    }
+                } else {
+                    res.send("The brief of this marker is not written yet, visit sometimes later, till then keep exploring other markers, thank you!");
+                }
+            });
+        } else {
+            console.log("Following error occured while retrieving the brief data: " + briefErr);
+        }
+    });
 });
 
 module.exports = router;
