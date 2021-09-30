b97997cd3110d4036c4d3a6a03bac6ec65243936
Meet-Turakhia <turakhiameeturesh@gmail.com>
2021-09-30 18:00:35 +0530

    added copy url functionality

--- views/public/js/brief.js
+++ views/public/js/brief.js
@@ -22,3 +22,18 @@ function pauseAllVideos() {
         }
     });
 }
+
+
+// copy url function
+function copyUrl() {
+    var Url = document.getElementById("box");
+    Url.value = window.location.href;
+    Url.focus();
+    Url.select();
+    document.getElementById("custom-tooltip").style.display = "inline";
+    document.execCommand("copy");
+    setTimeout(function () {
+        document.getElementById("custom-tooltip").style.display = "none";
+    }, 1000);
+
+};
